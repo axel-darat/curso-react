73225921f3ca60aad857fd87a063b11196b745b0
Axel Darat <axeldarat@gmail.com>
2023-07-18 11:47:01 -0400

    primeras clases

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,14 @@
+// variables y constantes
+const nombre = "Axel";
+const apellido = "Darat";
+
+let valorDado = 5;
+valorDado = 4;
+console.log({nombre, apellido, valorDado})
+
+if (true) {
+    const nombre = 'Peter';
+    console.log(nombre, apellido);
+}
+
+console.log(valorDado);
---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,9 @@
+const firstname = 'Axel';
+const lastname = 'Darat';
+
+const fullname = `${firstname} 
+${lastname}
+${1+1}
+`;
+
+console.log(fullname)
